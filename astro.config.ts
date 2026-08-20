2cb8d507430834a8782d5cfea9dacbb931a78df6
Kombumori <96778966+FRLP@users.noreply.github.com>
2026-08-20 03:41:21 +0000

    chore: install cf adapter

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL ?? process.env.CF_PAGES_URL ?? "http://localhost:4321",
@@ -10,4 +12,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
